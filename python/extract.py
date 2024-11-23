af0fdcef5f223eb3b89e43ccb4e62e277b1c30b5
Aowss Ibrahim <aowss@yahoo.com>
2024-11-23 11:58:31 -0500

    Adding simple textractor sample

--- python/extract.py
+++ python/extract.py
@@ -1,6 +1,10 @@
 import pdfquery
 from lxml import etree
+
 from pypdf import PdfReader
+
+from textractor import Textractor
+from textractor.data.constants import TextractFeatures
 
 input_dir = 'resources/in'
 output_dir = 'resources/out'
@@ -9,6 +13,7 @@ def pdf_extract(input_file_name: str):
     in_path = f'{input_dir}/{input_file_name}.pdf'
     write_to_file(extract_pdfquery(in_path), f'{input_file_name}-pdfquery.xml')
     write_to_file(extract_pdfreader(in_path), f'{input_file_name}-pdfreader.txt')
+    write_to_file(extract_textractor(in_path), f'{input_file_name}-textractor.json')
 
 
 def write_to_file(output: str, file_name: str):
@@ -32,7 +37,23 @@ def extract_pdfreader(input_path: str):
     return text
 
 
-if __name__ == '__main__':
-    file_name = 'ISCC-Sample-1'
-    pdf_extract(file_name)
+def extract_textractor(input_path: str):
+    extractor = Textractor(profile_name="default")
+    # Text extraction
+    # document = extractor.detect_document_text(file_source=input_path)
+    # return document.text
 
+    # analyze_document doesn't work when there is more than 1 page
+    document = extractor.analyze_document(
+        file_source=input_path,
+        features=[TextractFeatures.FORMS]
+    )
+    return document.key_values.pretty_print()
+
+    # start_document_analysis requires the path to be an S3 path
+
+
+
+if __name__ == '__main__':
+    file_name = 'ISCC-Sample-2'
+    pdf_extract(file_name)
